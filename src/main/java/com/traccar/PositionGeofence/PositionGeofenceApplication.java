e048d9385d84b2cab9328cd32085b319f55331cb
Josue Bladimir Morales Llanganate <76928507+MrT-coder@users.noreply.github.com>
2025-03-25 12:21:04 -0500

    Levantando Proyecto SpringBoot

---
+++ src/main/java/com/traccar/PositionGeofence/PositionGeofenceApplication.java
@@ -0,0 +1,13 @@
+package com.traccar.PositionGeofence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionGeofenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionGeofenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traccar/PositionGeofence/PositionGeofenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traccar.PositionGeofence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionGeofenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
